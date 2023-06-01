9ead11b042a7f22944e824cf66384964763f32d2
castlectry <ksturtle2@naver.com>
2023-06-01 17:33:59 +0900

    feat: Board CRUD, Logging fix: JDBCTemplate

--- src/main/java/com/example/demo/controller/Prac02Controller.java
+++ src/main/java/com/example/demo/controller/Prac02Controller.java
@@ -2,30 +2,72 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Board;
 import com.example.demo.service.Prac02Service;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController         // Rest API ( responsebody 생략 가능 )
 @RequestMapping("/boards")
+@Slf4j
 public class Prac02Controller {
 
     private final Prac02Service service;
 
     @Autowired
-    public Prac02Controller(Prac02Service service){
+    public Prac02Controller(Prac02Service service) {
         this.service = service;
     }
 
-    @PostMapping
-    public Board createBoard(@RequestBody Board board){
+    /* C = PostMapping , R = GetMapping, U = PutMapping, D = DeleteMapping */
+
+    @PostMapping("")
+    public String createBoard(@RequestBody Board board) {
+        log.debug("createBoard Board : {}.", board);
+
         // 게시판 등록
-        return service.createBoard(board);
+
+//        System.out.println(board.toString());
+
+        boolean isSuccess = service.createBoard(board);
+
+        // ExceptionHandling으로 추후에 바꾸자 ..
+        if(isSuccess){
+            return "OK";
+        }
+        return "check";
     }
 
-    @GetMapping("/{id}")
-    public Board getBoard(@PathVariable int bbsseq){
+    @GetMapping("/{bbsseq}")
+    public Board getBoard(@PathVariable int bbsseq) {
+        log.debug("getBoard bbsseq : {}.", bbsseq);
+        System.out.println("getBoard : " + bbsseq);
         // 게시판 상세보기
         return service.getBoard(bbsseq);
     }
+
+    @PutMapping("/{bbsseq}")
+    public String reWriteBoard(@RequestBody Board board, @PathVariable int bbsseq){
+        log.debug("reWriteBoard Board : {}.", board);
+        System.out.println("reWriteBoard : " + board.toString() + bbsseq);
+
+        boolean isSuccess = service.reWriteBoard(board, bbsseq);
+
+        if(isSuccess){
+            return "OK";
+        }
+        return "null";
+    }
+
+    @DeleteMapping("/{bbsseq}")
+    public String deleteBoard(@PathVariable int bbsseq){
+        log.debug("deleteBoard : {}.", bbsseq);
+        boolean isSuccess = service.deleteBoard(bbsseq);
+        if(isSuccess){
+            return "OK";
+        }
+        return "null";
+    }
+
+
 
 }
--- src/main/java/com/example/demo/model/Board.java
+++ src/main/java/com/example/demo/model/Board.java
@@ -2,6 +2,13 @@ package com.example.demo.model;
 
 import lombok.*;
 
+
+/*
+    Getter Setter
+    AllArgsConstructor
+    NoArgsConstructor
+*/
+
 @Data
 public class Board {
     private int bbsseq;
