c1a97d14fb4a05e2e6cfd2db26d92a90f3ab0d4a
newandthe <ksturtle2@naver.com>
2023-06-13 00:22:01 +0900

    feat: MethodArgumentTypeMismatchException 추가, 콘트롤러 응답내용 추가 (CUD)

--- src/main/java/com/example/demo/controller/Prac02Controller.java
+++ src/main/java/com/example/demo/controller/Prac02Controller.java
@@ -6,6 +6,8 @@ import com.example.demo.model.SearchParam;
 import com.example.demo.service.Prac02Service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -28,64 +30,97 @@ public class Prac02Controller {
 
     /* C = PostMapping , R = GetMapping, U = PutMapping, D = DeleteMapping */
 
-    @PostMapping("/write")        // Create
-    public Map<String,String> createBoard(@Valid @RequestBody Board board) {
-        log.debug("createBoard Board : {}.", board);
-
-
+    @PostMapping("/write") // Create
+    public ResponseEntity<Map<String, String>> createBoard(@Valid @RequestBody Board board) {
+        log.debug("createBoard Board: {}.", board);
 
         // 게시판 등록
         boolean isSuccess = service.createBoard(board);
 
-        if(isSuccess){
+        if (isSuccess) {
             HashMap<String, String> result = new HashMap<>();
-
             result.put("message", "게시물 등록 성공");
-            String str = String.valueOf(LocalDateTime.now());
-            result.put("timestamp", str);
+            String timestamp = String.valueOf(LocalDateTime.now());
+            result.put("timestamp", timestamp);
+            result.put("status", String.valueOf(HttpStatus.CREATED.value()));
 
-
-
-
-            return Collections.singletonMap("message", "게시물 등록 성공");
+            return ResponseEntity.status(HttpStatus.CREATED).body(result);
         }
-        return Collections.singletonMap("message", "게시물 등록 실패");
+
+        HashMap<String, String> result = new HashMap<>();
+        result.put("message", "게시물 등록 실패");
+        result.put("status", String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()));
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
     }
 
-    @GetMapping("/{bbsseq}")     // Read
-    public Board getBoard(@PathVariable int bbsseq) {
-        log.debug("getBoard bbsseq : {}.", bbsseq);
+    @GetMapping("/{bbsseq}") // Read
+    public ResponseEntity<Board> getBoard(@PathVariable int bbsseq) {
+        log.debug("getBoard bbsseq: {}.", bbsseq);
         // 게시판 상세보기
-        return service.getBoard(bbsseq);
+        Board board = service.getBoard(bbsseq);
+        if (board != null) {
+            return ResponseEntity.ok(board);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 
 
-    @PutMapping("/{bbsseq}")        // Update
-    public Map<String, String> reWriteBoard(@Valid @RequestBody Board board, @PathVariable int bbsseq){
-        log.debug("reWriteBoard Board : {}.", board);
+    @PutMapping("/{bbsseq}") // Update
+    public ResponseEntity<Map<String, String>> reWriteBoard(@Valid @RequestBody Board board, @PathVariable int bbsseq) {
+        log.debug("reWriteBoard Board: {}.", board);
 
         // 게시판 수정
         boolean isSuccess = service.reWriteBoard(board, bbsseq);
 
-        if(isSuccess){
-            return Collections.singletonMap("message", "게시물 수정 성공");
+        if (isSuccess) {
+            HashMap<String, String> result = new HashMap<>();
+            result.put("message", "게시물 수정 성공");
+            String timestamp = String.valueOf(LocalDateTime.now());
+            result.put("timestamp", timestamp);
+            result.put("status", String.valueOf(HttpStatus.OK.value()));
+
+            return ResponseEntity.ok(result);
+        } else {
+            HashMap<String, String> result = new HashMap<>();
+            result.put("message", "게시물 수정 실패");
+            String timestamp = String.valueOf(LocalDateTime.now());
+            result.put("timestamp", timestamp);
+            result.put("status", String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()));
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
         }
-        return Collections.singletonMap("message", "게시물 수정 실패");
     }
+
 
 
     // 존재하는지 먼저 파악한후 throw
-    @DeleteMapping("/{bbsseq}")     // Delete
-    public Map<String, String> deleteBoard(@PathVariable int bbsseq){
-        log.debug("deleteBoard : {}.", bbsseq);
+    @DeleteMapping("/{bbsseq}") // Delete
+    public ResponseEntity<Map<String, String>> deleteBoard(@PathVariable int bbsseq) {
+        log.debug("deleteBoard: {}.", bbsseq);
         // 게시판 삭제
         boolean isSuccess = service.deleteBoard(bbsseq);
-        if(isSuccess){
-            return Collections.singletonMap("message", "게시물 삭제 성공");
+        if (isSuccess) {
+            HashMap<String, String> result = new HashMap<>();
+            result.put("message", "게시물 삭제 성공");
+            String timestamp = String.valueOf(LocalDateTime.now());
+            result.put("timestamp", timestamp);
+            result.put("status", String.valueOf(HttpStatus.OK.value()));
+
+            return ResponseEntity.ok(result);
+        } else {
+            HashMap<String, String> result = new HashMap<>();
+            result.put("message", "게시물 삭제 실패");
+            String timestamp = String.valueOf(LocalDateTime.now());
+            result.put("timestamp", timestamp);
+            result.put("status", String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()));
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
         }
-        return Collections.singletonMap("message", "게시물 삭제 실패");
     }
+
 
     @GetMapping("/bbslist")         // Read    // Null 가능 // lombok DefaultValue
     public Map boardList(@RequestBody SearchParam searchparam){
